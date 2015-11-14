3c47206524a0dedb9fd5623c3ae8fbe37713c69b
Rohit Kulkarni <rkulkar1@stevens.edu>
2015-11-14 17:18:19 -0500

    Create Postscript.cc

---
+++ Postscript.cc
@@ -0,0 +1,89 @@
+// Authors: Rutuja Sawant, Rohit Kulkarni
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Postscript
+{
+ private:
+	ofstream file;
+ public:
+	Postscript(const char filename[])
+	{
+		file.open(filename);
+    	}
+
+    	void line()
+    	{
+    		//
+    	}
+
+    	void triangle()
+    	{
+    		//
+    	}
+
+    	void filltriangle()
+    	{
+    		//
+    	}
+
+    	void rect()
+    	{
+    		//
+    	}
+
+    	void fillrect()
+    	{
+    		//
+    	}
+
+    	void circle()
+    	{
+    		//
+    	}
+
+    	void fillcircle()
+    	{
+    		//
+    	}
+
+    	void polyline()
+    	{
+    		//
+    	}
+
+    	void grid()
+    	{
+    		//
+    	}
+
+    	void shadedgrid()
+    	{
+    		//
+    	}
+
+    	void text()
+    	{
+    		//
+    	}
+
+    	void filltext()
+    	{
+    		//
+    	}
+
+    	void lgraph()
+    	{
+    		//
+    	}
+
+    	void bgraph()
+    	{
+    		//
+    	}
+    };
+
+    int main()
+    {}
