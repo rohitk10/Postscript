7d3790c848d58094af6106cb2baba5ba6d0bb1ce
Rutuja Rajan Sawant <rsawant2@stevens.edu>
2015-11-28 12:31:59 -0500

    Update Postscript.cc

--- Postscript.cc
+++ Postscript.cc
@@ -150,7 +150,12 @@ class Postscript
     	{
     		//In the text function, we write various texts including company names and logos. Examples - Google, Dell, Adidas etc.
     	}
-
+	
+	void graphictext()
+	{
+		//
+	}
+	
     	void filltext()
     	{
     		//This function is used to give color to texts.
