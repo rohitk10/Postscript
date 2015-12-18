a17467728850f5e1bc4f01d0da68a0ba643c7c03
Rohit Kulkarni <rkulkar1@stevens.edu>
2015-12-18 12:10:00 -0500

    Update basicshapes.hh

--- Final_Project/basicshapes.hh
+++ Final_Project/basicshapes.hh
@@ -19,67 +19,58 @@ class Postscript
     	}
 
 	void setcolor(double r, double g, double b)
-	{
-		file<<r<<' '<<g<<' '<<b<<" setrgbcolor \n";
-	}
+		{
+			file<<r<<' '<<g<<' '<<b<<" setrgbcolor \n";
+		}
 
 	void selectcolor()
-	{
-		cout << "1. Red \n"
-		cout << "2. Green \n"
-		cout << "3. Blue \n"
-		cout << "4. Magenta \n"
-		cout << "5. Yellow \n"
-		cout << "6. Cyan \n"
-		cout << "7. Gray \n"
-		cout << "8. Black \n"
-		cout << "9. White \n"
-		cout << "Select your color \n";
-		int clr;
-		cin >> clr;
-		switch(clr)
 		{
-		case 1 :
-		setcolor(1, 0, 0);
-		break;
-		
-		case 2 :
-		setcolor(0, 1, 0);
-		break;
-					
-		case 3 :
-		setcolor(0, 0, 1);
-		break;
-					
-		case 4 :
-		setcolor(1, 0, 1);
-		break;
-					
-		case 5 :
-		setcolor(1, 1, 0);
-		break;
-		
-		case 6 :
-		setcolor(0, 1, 1);
-		break;
-		
-		case 7 :
-		setcolor(0, 0, 0);
-		setgray(0.5);
-		break;
-					
-		case 8 :
-		setcolor(0, 0, 0);
-		break;
-			
-		case 9 :
-		setcolor(1, 1, 1);
-		break;
-		
-		default :
-		cout << "Invalid Choice! \n";
+			        cout << "1. Red \n"
+					     << "2. Green \n"
+						 << "3. Blue \n"
+						 << "4. Magenta \n"
+						 << "5. Yellow \n"
+						 << "6. Cyan \n"
+						 << "7. Gray \n"
+						 << "8. Black \n"
+						 << "9. White \n"
+						 << "Select your color \n";
+					int clr;
+					cin >> clr;
+					switch(clr)
+					{
+					case 1 :
+						setcolor(1, 0, 0);
+						break;
+					case 2 :
+						setcolor(0, 1, 0);
+						break;
+					case 3 :
+						setcolor(0, 0, 1);
+						break;
+					case 4 :
+						setcolor(1, 0, 1);
+						break;
+					case 5 :
+						setcolor(1, 1, 0);
+						break;
+					case 6 :
+						setcolor(0, 1, 1);
+						break;
+					case 7 :
+						setcolor(0, 0, 0);
+						setgray(0.5);
+						break;
+					case 8 :
+						setcolor(0, 0, 0);
+						break;
+					case 9 :
+						setcolor(1, 1, 1);
+						break;
+					default :
+						cout << "Invalid Choice! \n";
+					}
 		}
-	}
 
 	void setgray(double n)
 		{
@@ -97,167 +88,167 @@ class Postscript
     	void polyline(int n)
     		{
     			double arr[2*(n + 1)];
-    			cout << "Enter the coordinates of the polyline one by one \n";
-    			for(int i = 0; i < 2*(n + 1); i++)
-    			{
-    				cin >> arr[i];
-    			}
+    					cout << "Enter the coordinates of the polyline one by one \n";
+    					for(int i = 0; i < 2*(n + 1); i++)
+    					{
+    						cin >> arr[i];
+    					}
 
-    			file << arr[0] << ' ' << arr[1] << ' ' << "moveto \n";
-    			for(int i = 2; i < 2*(n + 1); i = i + 2)
-    			{
-    				file << arr[i] << ' ' << arr[i+1] << ' ' << "lineto \n";
-    			}
-    			file << "stroke";
+    					file << arr[0] << ' ' << arr[1] << ' ' << "moveto \n";
+    					for(int i = 2; i < 2*(n + 1); i = i + 2)
+    					{
+    						file << arr[i] << ' ' << arr[i+1] << ' ' << "lineto \n";
+    					}
+    					file << "stroke";
     		}
 
     	void rect(double x, double y, double w, double h)
-    	{
-    		file<<x<<' '<< y <<" moveto \n";
-    	 	file<<x+w<<' '<<y<<" lineto \n";
-    		file<<x+w<<' '<<y+h<<" lineto \n";
-    		file<<x<<' '<<y+h<<" lineto \n";
-    		file<<"closepath \n";
-    		file<<"stroke \n";
-    	}
+    		{
+    		    file<<x<<' '<< y <<" moveto \n";
+    			file<<x+w<<' '<<y<<" lineto \n";
+    			file<<x+w<<' '<<y+h<<" lineto \n";
+    			file<<x<<' '<<y+h<<" lineto \n";
+    			file<<"closepath \n";
+    			file<<"stroke \n";
+    		}
 
     	void fillrect(double x, double y, double w, double h)
-    	{
-    		file<<x<<' '<< y <<" moveto \n";
-    		file<<x+w<<' '<<y<<" lineto \n";
-    		file<<x+w<<' '<<y+h<<" lineto \n";
-    		file<<x<<' '<<y+h<<" lineto \n";
-    		file<<"closepath \n";
-    		file<<"fill \n";
-    	}
+    		{
+    		        file<<x<<' '<< y <<" moveto \n";
+    			file<<x+w<<' '<<y<<" lineto \n";
+    			file<<x+w<<' '<<y+h<<" lineto \n";
+    			file<<x<<' '<<y+h<<" lineto \n";
+    			file<<"closepath \n";
+    			file<<"fill \n";
+    		}
 
     	void circle(double x, double y, double r)
-    	{
-    	//In this function we draw a circle with (x,y) as the centre and radius r obtained from the user.
-    		file << x << ' ' << y << ' ' << r << ' ' << "0" << ' ' << "360" << ' ' << "arc \n";
-    		file<<"stroke \n";
-    	}
+    		    	{
+    		    		//In this function we draw a circle with (x,y) as the centre and radius r obtained from the user.
+    		    		file << x << ' ' << y << ' ' << r << ' ' << "0" << ' ' << "360" << ' ' << "arc \n";
+    		    		file<<"stroke \n";
+    		    	}
 
-    	void fillcircle(double x, double y, double r)
-    	{
-    	//In this function we draw and fill a circle with (x,y) as the center and radius r obtained from the user.
-    		file << x << ' ' << y << ' ' << r << ' ' << "0" << ' ' << "360" << ' ' << "arc \n";
-    		file<<"fill \n";
-    	}
+    		void fillcircle(double x, double y, double r)
+    			    	{
+    			    		//In this function we draw and fill a circle with (x,y) as the center and radius r obtained from the user.
+    			    		file << x << ' ' << y << ' ' << r << ' ' << "0" << ' ' << "360" << ' ' << "arc \n";
+    			    		file<<"fill \n";
+    			    	}
 
-    	void triangle(double x1, double y1, double x2, double y2, double x3, double y3)
-    	{
-    		file<<x1<<' '<<y1<<" moveto \n";
-    		file<<x2<<' '<<y2<<" lineto \n";
-    		file<<x3<<' '<<y3<<" lineto \n";
-    		file<<"closepath \n";
-    		file<<"stroke \n";
-    	}
+    		void triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+    			{
+    				file<<x1<<' '<<y1<<" moveto \n";
+    				file<<x2<<' '<<y2<<" lineto \n";
+    				file<<x3<<' '<<y3<<" lineto \n";
+    				file<<"closepath \n";
+    				file<<"stroke \n";
+    			}
 
-    	void filltriangle(double x1, double y1, double x2, double y2, double x3, double y3)
-    	{
-    		file<<x1<<' '<<y1<<" moveto \n";
-    		file<<x2<<' '<<y2<<" lineto \n";
-    		file<<x3<<' '<<y3<<" lineto \n";
-    		file<<"closepath \n";
-    		file<<"fill \n";
-    	}
+    			void filltriangle(double x1, double y1, double x2, double y2, double x3, double y3)
+    				{
+    					file<<x1<<' '<<y1<<" moveto \n";
+    					file<<x2<<' '<<y2<<" lineto \n";
+    					file<<x3<<' '<<y3<<" lineto \n";
+    					file<<"closepath \n";
+    					file<<"fill \n";
+    				}
 
-    	void ellipse(double x, double y, double r1, double r2)
-    	{
-    		double x0, y0;
-    		x0 = x;
-    		y0 = y - r2 / 2;
-    		file << "gsave \n";
-    		file << x0 << ' ' << y0 << ' ' << "translate \n";
-    		file << "0 0 moveto \n";
-    		file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
-    		file << "180 rotate \n";
-    		file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
-    		file << "closepath \n";
-    		file << "stroke \n";
-    		file << "grestore \n";
-    	}
+    			void ellipse(double x, double y, double r1, double r2)
+    				{
+    					double x0, y0;
+    					x0 = x;
+    					y0 = y - r2 / 2;
+    					file << "gsave \n";
+    					file << x0 << ' ' << y0 << ' ' << "translate \n";
+    					file << "0 0 moveto \n";
+    					file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
+    					file << "180 rotate \n";
+    					file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
+    					file << "closepath \n";
+    					file << "stroke \n";
+    					file << "grestore \n";
+    				}
 
-    	void fillellipse(double x, double y, double r1, double r2)
-    	{
-    		double x0, y0;
-    		x0 = x;
-    		y0 = y - r2 / 2;
-    		file << "gsave \n";
-    		file << x0 << ' ' << y0 << ' ' << "translate \n";
-    		file << "0 0 moveto \n";
-    		file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
-    		file << "180 rotate \n";
-    		file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
-    		file << "closepath \n";
-    		file << "fill \n";
-    		file << "grestore \n";
-    	}
+    				void fillellipse(double x, double y, double r1, double r2)
+    					{
+    						double x0, y0;
+    						x0 = x;
+    						y0 = y - r2 / 2;
+    						file << "gsave \n";
+    						file << x0 << ' ' << y0 << ' ' << "translate \n";
+    						file << "0 0 moveto \n";
+    						file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
+    						file << "180 rotate \n";
+    						file << r1/2 << ' ' << '0' << ' ' << r1/2 << ' ' << r2 << ' ' << '0' << ' ' << r2 << ' ' << "rcurveto \n";
+    						file << "closepath \n";
+    						file << "fill \n";
+    						file << "grestore \n";
+    					}
 
-    	void star(double x, double y, double s)
-    	{
-    		file << "gsave \n"
-    		file << "/side { \n"
-    		file << s << ' ' << "0 lineto \n"
-    		file << "currentpoint translate \n"
-    		file << "-144 rotate \n"
-    		file << "} def \n"
-    		file << "/star { \n"
-    		file << "currentpoint translate \n"
-    		file << "4 {side} repeat \n"
-    		file << "closepath \n"
-    		file << "} def \n"
-    		file << x << ' ' << y << ' ' << "moveto \n"
-    		file << "star \n"
-    		file << "stroke \n"
-    		file << "grestore \n";
-    	}
+    				void star(double x, double y, double s)
+    					{
+    						file << "gsave \n"
+    						     << "/side { \n"
+    						     << s << ' ' << "0 lineto \n"
+    						     << "currentpoint translate \n"
+    						     << "-144 rotate \n"
+    						     << "} def \n"
+    						     << "/star { \n"
+    						     << "currentpoint translate \n"
+    						     << "4 {side} repeat \n"
+    						     << "closepath \n"
+    						     << "} def \n"
+    						     << x << ' ' << y << ' ' << "moveto \n"
+    						     << "star \n"
+    						     << "stroke \n"
+    						     << "grestore \n";
+    					}
 
-    	void fillstar(double x, double y, double s)
-    	{
-    		file << "gsave \n";
-    		file << "/side { \n"
-    		file << s << ' ' << "0 lineto \n"
-    		file << "currentpoint translate \n"
-    		file << "-144 rotate \n"
-    		file << "} def \n"
-    		file << "/star { \n"
-    		file << "currentpoint translate \n"
-    		file << "4 {side} repeat \n"
-    		file << "closepath \n"
-    		file << "} def \n"
-    		file << x << ' ' << y << ' ' << "moveto \n"
-    		file << "star \n"
-    		file << "fill \n"
-    		file << "grestore \n";
-    	}
+    					void fillstar(double x, double y, double s)
+    						{
+    						    file << "gsave \n";
+    							file << "/side { \n"
+    							     << s << ' ' << "0 lineto \n"
+    							     << "currentpoint translate \n"
+    							     << "-144 rotate \n"
+    							     << "} def \n"
+    							     << "/star { \n"
+    							     << "currentpoint translate \n"
+    							     << "4 {side} repeat \n"
+    							     << "closepath \n"
+    							     << "} def \n"
+    							     << x << ' ' << y << ' ' << "moveto \n"
+    							     << "star \n"
+    							     << "fill \n"
+    								 << "grestore \n";
+    						}
 
-    	void polygon(double x, double y, int n, double l)
-    	{
-    		double ang = 360 / n;
-    		file << "gsave \n";
-    		file << x << ' ' << y << ' ' << "translate \n"
-    		file << "0 0 moveto \n"
-    		file << n << " {" << l << " 0 lineto currentpoint translate " << ang << " rotate} repeat \n"
-    		file << "closepath \n"
-    		file << "stroke \n"
-    		file << "grestore \n";
-    	}
+    					void polygon(double x, double y, int n, double l)
+    						{
+    							double ang = 360 / n;
+    							file << "gsave \n";
+    							file << x << ' ' << y << ' ' << "translate \n"
+    								 << "0 0 moveto \n"
+    								 << n << " {" << l << " 0 lineto currentpoint translate " << ang << " rotate} repeat \n"
+    								 << "closepath \n"
+    								 << "stroke \n"
+    								 << "grestore \n";
+    						}
 
-    	void fillpolygon(double x, double y, int n, double l)
-    	{
-    		double ang = 360 / n;
-    		file << "gsave \n";
-    		file << x << ' ' << y << ' ' << "translate \n"
-    		file << "0 0 moveto \n"
-    		file << n << " {" << l << " 0 lineto currentpoint translate " << ang << " rotate} repeat \n"
-    		file << "closepath \n"
-    		file << "fill \n"
-    		file << "grestore \n";
-    	}
+    						void fillpolygon(double x, double y, int n, double l)
+    							{
+    								double ang = 360 / n;
+    								file << "gsave \n";
+    								file << x << ' ' << y << ' ' << "translate \n"
+    									 << "0 0 moveto \n"
+    									 << n << " {" << l << " 0 lineto currentpoint translate " << ang << " rotate} repeat \n"
+    									 << "closepath \n"
+    									 << "fill \n"
+    									 << "grestore \n";
+    							}
 
-	void closefile()
+	    	void closefile()
 	{
 		file.close();
 	}
