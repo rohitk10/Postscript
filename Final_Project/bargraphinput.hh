12e1186d1b599c7f5ee8e4ea9ee1120639da1a3b
Rutuja Rajan Sawant <rsawant2@stevens.edu>
2015-12-17 21:38:13 -0500

    Update bargraphinput.hh

--- Final_Project/bargraphinput.hh
+++ Final_Project/bargraphinput.hh
@@ -10,86 +10,86 @@ void bar_input()
 {
 	BarGraph b("bargraph.ps");
 	string title, h_label, v_label;
-				int num_of_values,num_of_variables;
-				cout << "Enter Graph Title \n";
-				cin >> title;
-				cout << "Enter Horizontal axis label and Vertical axis label \n";
-				cin >> h_label;
-				cin >> v_label;
-				cout << "Enter number of bars (1 or 2) \n";
-				cin >> num_of_variables;
-				switch(num_of_variables)
+	int num_of_values,num_of_variables;
+	cout << "Enter Graph Title \n";
+	cin >> title;
+	cout << "Enter Horizontal axis label and Vertical axis label \n";
+	cin >> h_label;
+	cin >> v_label;
+	cout << "Enter number of bars (1 or 2) \n";
+	cin >> num_of_variables;
+	switch(num_of_variables)
+	{
+		case 1 :
+		{
+			cout << "Enter number of values on horizontal axis \n";
+			cin >> num_of_values;
+			double b1[num_of_values];
+			string label[num_of_values];
+			double intervalh;
+			intervalh = 500 / num_of_values;
+			for(int i = 0; i < num_of_values; i ++)
+			{
+				cout << "Enter label " << i+1 << '\n';
+				cin >> label[i];
+				cout << "Enter value " << i+1 << '\n';
+				cin >> b1[i];
+			}
+			double max;
+			max = b1[0];
+			for(int i = 0; i < num_of_values-1; i++)
+			{
+				if(b1[i+1] > max)
 				{
-				case 1 :
-				{
-					cout << "Enter number of values on horizontal axis \n";
-					cin >> num_of_values;
-					double b1[num_of_values];
-					string label[num_of_values];
-					double intervalh;
-					intervalh = 500 / num_of_values;
-					for(int i = 0; i < num_of_values; i ++)
-					{
-						cout << "Enter label " << i+1 << '\n';
-						cin >> label[i];
-						cout << "Enter value " << i+1 << '\n';
-						cin >> b1[i];
-					}
-					double max;
-					max = b1[0];
-					for(int i = 0; i < num_of_values-1; i++)
-					{
-						if(b1[i+1] > max)
-						{
-							max = b1[i+1];
-						}
-					}
-					double intervalv;
-					intervalv = 500/max;
-					b.bargraph1(title, h_label, v_label, num_of_values, intervalh, intervalv, label, b1);
-					break;
+					max = b1[i+1];
 				}
+			}
+			double intervalv;
+			intervalv = 500/max;
+			b.bargraph1(title, h_label, v_label, num_of_values, intervalh, intervalv, label, b1);
+			break;
+		}
 
-				case 2 :
+		case 2 :
+		{
+			cout << "Enter number of values on horizontal axis \n";
+			cin >> num_of_values;
+			double b1[num_of_values];
+			string label[num_of_values];
+			double intervalh;
+			intervalh = 500 / num_of_values;
+			double b2[num_of_values];
+			for(int i = 0; i < num_of_values; i ++)
+			{
+				cout << "Enter label " << i+1 << '\n';
+				cin >> label[i];
+			}
+			for(int i = 0; i < num_of_values; i ++)
+			{
+				cout << "Enter value " << i+1 << " for Bar 1 " << '\n';
+				cin >> b1[i];
+				cout << "Enter value " << i+1 << " for Bar 2 " << '\n';
+				cin >> b2[i];
+			}
+			double max;
+			max = b1[0];
+			for(int i = 0; i < num_of_values-1; i++)
+			{
+				if(b1[i+1] > max)
 				{
-					cout << "Enter number of values on horizontal axis \n";
-					cin >> num_of_values;
-					double b1[num_of_values];
-					string label[num_of_values];
-					double intervalh;
-					intervalh = 500 / num_of_values;
-					double b2[num_of_values];
-					for(int i = 0; i < num_of_values; i ++)
-					{
-						cout << "Enter label " << i+1 << '\n';
-						cin >> label[i];
-					}
-					for(int i = 0; i < num_of_values; i ++)
-					{
-						cout << "Enter value " << i+1 << " for Bar 1 " << '\n';
-						cin >> b1[i];
-						cout << "Enter value " << i+1 << " for Bar 2 " << '\n';
-						cin >> b2[i];
-					}
-					double max;
-					max = b1[0];
-					for(int i = 0; i < num_of_values-1; i++)
-					{
-						if(b1[i+1] > max)
-						{
-							max = b1[i+1];
-						}
-						if(b2[i+1] > max)
-						{
-							max = b2[i+1];
-						}
-					}
-					double intervalv;
-					intervalv = 500 / max;
-					b.bargraph2(title, h_label, v_label, num_of_values, intervalh, intervalv, label, b1, b2);
-					break;
+					max = b1[i+1];
 				}
+				if(b2[i+1] > max)
+				{
+					max = b2[i+1];
 				}
+			}
+			double intervalv;
+			intervalv = 500 / max;
+			b.bargraph2(title, h_label, v_label, num_of_values, intervalh, intervalv, label, b1, b2);
+			break;
+		}
+	}
 }
 
 #endif
