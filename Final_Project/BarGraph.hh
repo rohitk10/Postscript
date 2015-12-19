22f8be1df204319e9a2e3da25fb63839261dd1d8
Rohit Kulkarni <rkulkar1@stevens.edu>
2015-12-19 10:39:01 -0500

    added documentation

--- Final_Project/BarGraph.hh
+++ Final_Project/BarGraph.hh
@@ -1,4 +1,9 @@
-//Author : Rohit Kulkarni
+/*Author : Rohit Kulkarni
+Description : This is a header file containing class BarGraph.
+              It has 2 methods that print a bar graph of either one or two variables
+              depending on the user's input.
+*/
+
 #ifndef _BARGRAPH_HH
 #define _BARGRAPH_HH
 
@@ -22,10 +27,12 @@ private:
 		bar.open(filename);
         }
         
-        //this function prints a bar graph containing one variable using postscript command
 
 	void bargraph1(string title, string h_label, string v_label, int num_of_variables, double intervalh, double intervalv, string label[], double b1[])
 	{
+		
+	//this function prints a bar graph containing one variable using postscript commands.
+	
 	                                             bar << "30 30 moveto \n"
 	    	                                         << "550 30 lineto \n"
 			                                 << "30 30 moveto \n"
@@ -44,15 +51,15 @@ private:
 							 << "90 rotate \n"
 							 << "(" << v_label << ") show \n"
 							 << "grestore \n"
-						     << "/Helvetica findfont \n"
+						         << "/Helvetica findfont \n"
 							 << "8 scalefont setfont \n";
 							 
-							 // for loop to display labels on the x-axis
+						// for loop to display labels on the x-axis
 							 
 						for(int i = 0; i < num_of_variables; i++)
 						{
 							bar << 30+((i+1)*intervalh) << " 22 moveto \n"
-								 << "(" << label[i] << ")" << " show \n";
+						            << "(" << label[i] << ")" << " show \n";
 						}
 						
 						// for loop to display n number of bars
@@ -64,23 +71,23 @@ private:
 						
 						// for loop to display corresponding values on y-axis
 						
-					    for(int i = 0; i < num_of_variables; i++)
-					    {
-					    	bar << "gsave \n"
-							     << "28 " << (b1[i] * intervalv) + 25<< " moveto \n"
-								 << "90 rotate \n"
-					    		 << "(" << b1[i] << ") show \n"
-								 << "grestore \n";
-					    }
+					        for(int i = 0; i < num_of_variables; i++)
+					        {
+					        	bar << "gsave \n"
+							    << "28 " << (b1[i] * intervalv) + 25<< " moveto \n"
+						            << "90 rotate \n"
+					    		    << "(" << b1[i] << ") show \n"
+							    << "grestore \n";
+					        }
 
-					}
+	}
 
-					void bargraph2(string title, string hl, string vl, int num_of_variables, double intervalh, double intervalv, string label[], double b1[], double b2[])
-					{
+	void bargraph2(string title, string hl, string vl, int num_of_variables, double intervalh, double intervalv, string label[], double b1[], double b2[])
+	{
 						
-						//this function prints a bargraph containing 2 variables using postscript commands
+	//this function prints a bargraph containing 2 variables using postscript commands
 						
-						bar << "30 30 moveto \n"
+					             bar << "30 30 moveto \n"
 							 << "550 30 lineto \n"
 							 << "30 30 moveto \n"
 							 << "30 550 lineto \n"
@@ -98,14 +105,14 @@ private:
 							 << "90 rotate \n"
 							 << "(" << vl << ") show \n"
 							 << "grestore \n"
-						     << "/Helvetica findfont \n"
+						         << "/Helvetica findfont \n"
 							 << "8 scalefont setfont \n";
 							 
-							 // for loop to display labels on x axis
+						// for loop to display labels on x axis
 							 
 						for(int i = 0; i < num_of_variables; i++)
 						{
-							bar << 30+((i+1)*intervalh) << " 22 moveto \n"
+							     bar << 30+((i+1)*intervalh) << " 22 moveto \n"
 								 << "(" << label[i] << ")" << " show \n";
 						}
 						
@@ -135,24 +142,24 @@ private:
 						
 						bar << "1 0 0 setrgbcolor \n";
 						for(int i = 0; i < num_of_variables; i++)
-					    {
+					        {
 							bar << "gsave \n"
-							     << "28 " << (b1[i] * intervalv) + 25<< " moveto \n"
-								 << "90 rotate \n"
-						   		 << "(" << b1[i] << ") show \n"
-								 << "grestore \n";
-				   	    }
+							    << "28 " << (b1[i] * intervalv) + 25<< " moveto \n"
+							    << "90 rotate \n"
+						   	    << "(" << b1[i] << ") show \n"
+							    << "grestore \n";
+				   	        }
 						bar << "0 0 1 setrgbcolor \n";
 						for(int i = 0; i < num_of_variables; i++)
 						{
 							bar << "gsave \n"
-							     << "28 " << (b2[i] * intervalv) + 25<< " moveto \n"
-								 << "90 rotate \n"
-						   		 << "(" << b2[i] << ") show \n"
-								 << "grestore \n";
-				   	    }
+							    << "28 " << (b2[i] * intervalv) + 25<< " moveto \n"
+							    << "90 rotate \n"
+						   	    << "(" << b2[i] << ") show \n"
+							    << "grestore \n";
+				   	        }
 
-					}
+	}
 
 					void closefile()
 						{
